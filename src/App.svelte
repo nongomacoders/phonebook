646c924d451c134dc117c2e66b972635aca58f1d
asusdion <dr.dtdavan@gmail.com>
2020-07-21 20:00:28 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	let contacts=['Diketso','Dr D','Elon']
+</script>
+
+<main>
+{#each contacts as contact}
+	<div class="text-purple-600 bg-black">{contact}</div>
+{/each}
+</main>
+<style>
+</style>
+
