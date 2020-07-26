5d65a1aca29fb08d3bcbe92598d8ebb59969fc12
Diketso Setho <diketso.setho16@gmail.com>
2020-07-26 15:46:03 +0200

    Added delete button. added a add contact button.(Still under devepment) Created a modal which is to be fired when adding new contact. Modal will contain inputs for contact credentials.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,45 @@
 <script>
 	import Contact from './Contact.svelte';
+	import Modal from './Modal.svelte';
+	let showModal = false;
+
+	const toggleAdd = () => {
+		showModal = !showModal;
+		console.log("Clicked");
+	}
 </script>
 
+
+
 <!--Displaying the imported contact component-->
-<Contact />
+<Modal {showModal}/>
+<main>
+	
+	<h1>PhoneBook using Svelt Js</h1>
+	
+	<Contact/>
+	<div style="padding:10px"></div>
+	<button on:click={toggleAdd}>Add</button>
+</main>
+
+
+<style>
+main{
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	@media (min-width: 640px){
+		main{
+			max-width: none;
+		}
+	}
+
+	h1{
+	text-align: center;
+	font-family: 'Rowdies', cursive;
+	font-size: auto;
+}
+</style>
 
