97914e002272425370dae1de7b23aa6458915aeb
Diketso Setho <diketso.setho16@gmail.com>
2020-08-01 17:13:48 +0200

    used prop in the js instead of array

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,23 @@
 <script>
 	import Modal from './Modal.svelte';
 	import AddContact from './AddContact.svelte';
+	//import Contact from './Contact.svelte';
+	
+	//export let id;
+	export let name;
+	export let surname;
+	export let cell;
+	export let email; 
 
-		//Test
-	let contacts = [
+	//Contact
+	export let contacts = [
 		{
 	id: 1,
-	name: "Diketso",
-	surname: "Setho",
-	cell: "084 308 8893",
-	email: "Diketso.setho16@gmail.com"
-	}
+	name: "",
+	surname: "",
+	cell: "",
+	email: ""
+		}
 	];
 
 	let showModal = false;
@@ -25,12 +32,10 @@
 		showModal = false;
 	}
 
-
-
 const deleteContact = (id) =>{
 	contacts = contacts.filter((contact) => contact.id != id);
-	
 }
+
 </script>
 
 
@@ -43,19 +48,20 @@ const deleteContact = (id) =>{
 
 <main>
 	<h1 class="text-5xl m-5">PhoneBook using Svelte Js</h1>
+		<p>{name}</p>
 
-
-	{#each contacts as contact (contact.email)}
+	{#each contacts as contact (contact.id)}
+		{#if contacts}
 			<div class="flex flex-wrap flex-shrink sm:flex-shrink-0 md:flex-shrink lg:flex-shrink-0 xl:flex-shrink">
-				<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{contact.name}</div>
-				<div class="w-1/5 bg-gray-400 h-12" style="padding: 10px; border:1px solid black">{contact.surname}</div>
-				<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{contact.cell}</div>
-				<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{contact.email}</div>
-				<div><button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-800 m-3" on:click={() => deleteContact(contact.id)}>X</button></div>
-					
-			</div>
-			{:else}
-		<h4 style="text-align:center text-lg">No contact to show...</h4>
+			<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{name}</div>
+			<div class="w-1/5 bg-gray-400 h-12" style="padding: 10px; border:1px solid black">{surname}</div>
+			<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{cell}</div>
+			<div class="w-1/5 bg-gray-500 h-12" style="padding: 10px; border:1px solid black">{email}</div>
+			<div><button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-800 m-3" on:click={() => deleteContact(contact.id)}>X</button></div>	
+		</div>
+		{:else}
+			<h4 style="text-align:center text-lg">No contact to show...</h4>
+		{/if}
 	{/each} 
 
 	<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" on:click={toggleModal}>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body	
+	target: document.body,
+	props:{
+		//id: 1,
+		name: 'Hello',
+		surname: 'Setho',
+		cell: '084 308 8893',
+		email: 'sdsd@sd.com'
+	}	
 });
 
 export default app;
